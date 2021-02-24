f126a7355e4fa78f689ba9802b8faa0c419fcdfa
Janis <janismcluckie@googlemail.com>
2021-02-24 12:07:16 +0000

    Amended file paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Card from './components/Card'
+import Card from './components/item/Card/Card'
+import Footer from './components/footer/Footer'
 
 const App = () => {
     return (
         <div className="App">
+            <Footer/>
             <Card img = "Img1" name = "Silvester" img2 = "Kiss"/>
         </div>
     );
--- src/components/item/Logo/Logo.js
+++ src/components/item/Logo/Logo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from './image/Image'
+import Image from '../../image/Image'
     
 const Logo = (props) => {
     return (
