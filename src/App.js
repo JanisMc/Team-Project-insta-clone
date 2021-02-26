fc74fe436a61646333818967f45208b570ba1c6e
Janis <janismcluckie@googlemail.com>
2021-02-26 17:26:21 +0000

    Added app.css styling, some minor changes to components

--- src/App.js
+++ src/App.js
@@ -8,19 +8,34 @@ const App = () => {
     return (
         <>
         <div className="App">
-      
-            <Footer/>
-      
-            <Card imgProfile = "Img1" name = "seekerofthecosmos" imgPost= "VR"/>
-            <Card imgProfile = "Img2" name = "pepeviyuela" imgPost= "Ball"/>
-            <Card imgProfile = "Img3" name = "get.me.now" imgPost= "Blue"/>
-            <Card imgProfile = "Img4" name = "donnushka.p" imgPost= "Kiss"/>
-            <Card imgProfile = "Img5" name = "salem.aka.cat" imgPost= "Laptop"/>
-            <Card imgProfile = "Img6" name = "muyayo.appareal" imgPost= "Robot1"/>
-            <Card imgProfile = "Img7" name = "hotsince82" imgPost= "Robot2"/>
-            <Card imgProfile = "Img8" name = "tekno.addict" imgPost= "Sad"/>
-           
-            <Sidebar/>
+
+            {/* <div>
+                <Header = "header"/>
+            </div> */}
+            <div className = "main">
+
+                <div className ="leftSide">
+
+                    <div className = "footer">
+                        <Footer/>
+                    </div>
+
+                    <div className = "feed">
+                        <Card imgProfile = "Img1" name = "seekerofthecosmos" imgPost= "VR"/>
+                        <Card imgProfile = "Img2" name = "pepeviyuela" imgPost= "Ball"/>
+                        <Card imgProfile = "Img3" name = "get.me.now" imgPost= "Blue"/>
+                        <Card imgProfile = "Img4" name = "donnushka.p" imgPost= "Kiss"/>
+                        <Card imgProfile = "Img5" name = "salem.aka.cat" imgPost= "Laptop"/>
+                        <Card imgProfile = "Img6" name = "muyayo.appareal" imgPost= "Robot1"/>
+                        <Card imgProfile = "Img7" name = "hotsince82" imgPost= "Robot2"/>
+                        <Card imgProfile = "Img8" name = "tekno.addict" imgPost= "Sad"/>
+                    </div>
+                </div>
+
+                <div className = "rightSide">
+                    <Sidebar/>
+                </div>
+            </div>
       
         </div>
     </>
